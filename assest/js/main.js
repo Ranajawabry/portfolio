5d16a36a4148a967e5983d60a02a030a936bd714
Ranajawabry <rana.a.jawabry@gmail.com>
2024-06-09 09:46:11 +0300

    update body height

--- assest/js/main.js
+++ assest/js/main.js
@@ -2,12 +2,15 @@ let body=document.body,
 loading=(console.log(body),document.querySelector(".loading"));loading.style.opacity=1,body.style.overflow="hidden",window.addEventListener("load",()=>{setInterval(()=>{loading.style.opacity=0,body.style.overflow="auto",loading.style.zIndex="0"},2e3)}),window.addEventListener("scroll",()=>{333<scrollY?document.getElementsByTagName("nav")[0].classList.add("new_nav"):document.getElementsByTagName("nav")[0].classList.remove("new_nav")});
 
 $('.theam i').click(()=>{
-    let width =$('.options').outerWidth()
+    
+    let width =$('.options').outerWidth() // return options width 
     console.log(width)
+    console.log($('.theam').offset().left)
+   
     if($('.theam').offset().left==0)
-       $('.theam').animate({left:-width},2000);
+       $('.theam').animate({left:-width},1500);
     else
-    $('.theam').animate({left:0},2000);
+    $('.theam').animate({left:0},1500);
 
 })
 
